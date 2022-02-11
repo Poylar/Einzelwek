635334c4db9b796baa9f7f4045442d69b98486eb
Poylar <doaleks8@gmail.com>
2022-02-11 13:41:11 +0800

    fix

---
+++ src/js/app.js
@@ -0,0 +1,102 @@
+import '../scss/app.scss';
+import { gsap } from 'gsap';
+
+function requireAll(r) {
+  // get all svg
+  r.keys().forEach(r);
+}
+requireAll(require.context('../images/svg/', true, /\.svg$/));
+(function () {
+  var throttle = function (type, name, obj) {
+    obj = obj || window;
+    var running = false;
+    var func = function () {
+      if (running) {
+        return;
+      }
+      running = true;
+      requestAnimationFrame(function () {
+        obj.dispatchEvent(new CustomEvent(name));
+        running = false;
+      });
+    };
+    obj.addEventListener(type, func);
+  };
+
+  /* init - you can init any event */
+  throttle('resize', 'optimizedResize');
+})();
+window.onload = () => {
+  const preloader = document.querySelector('.preloader');
+  const preloaderText = document.querySelector('.preloader__text');
+  const preloaderLetter = document.querySelector('.preloader__text_letter');
+  const preloaderLetterWidth = getComputedStyle(preloaderLetter).width;
+  const preloaderTextHidden = document.querySelector('.preloader__text_hidden');
+  console.log(preloaderLetter, preloaderLetterWidth);
+  preloaderText.style.width = preloaderLetterWidth;
+  preloader.querySelector('.preloader__text_hidden').classList.add('active');
+  const hiddenTextWidth = getComputedStyle(preloaderTextHidden).width;
+  setTimeout(() => {
+    preloaderText.style.width = `calc(${hiddenTextWidth} + ${preloaderLetterWidth} + 7px)`;
+    preloader.querySelector('.preloader__icon').classList.add('active');
+  }, 1000);
+  preloaderText.addEventListener('transitionend', () => {
+    const tl = gsap.timeline();
+    tl.to(preloader, {
+      yPercent: 3,
+    });
+    tl.to(preloader, {
+      yPercent: -100,
+      onComplete: function () {
+        const i = document.querySelectorAll('.text');
+        i.forEach((elem, index) => {
+          setTimeout(() => {
+            elem.style.transform = `translate3d(0px , 0%, 0px) rotate(0deg)`;
+          }, 70 * (index + 1));
+        });
+        document.querySelectorAll('.card__item').forEach((elem) => {
+          elem.classList.add('active');
+        });
+      },
+    });
+    tl.fromTo(
+      '.opacity-show',
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        stagger: 0.3,
+      }
+    );
+    tl.fromTo(
+      '.content',
+      { scale: 0 },
+      {
+        scale: 1,
+        onComplete: () => {
+          document.querySelector('.inner__video').play();
+        },
+      }
+    );
+  });
+};
+
+document.addEventListener('click', (e) => {
+  if (e.target.closest('.header__lang')) {
+    document.querySelector('.header__lang').classList.toggle('open');
+  }
+});
+
+function cutLangName() {
+  const langElem = document.querySelectorAll('[data-lang]');
+  if (window.innerWidth <= 556) {
+    langElem.forEach((elem) => {
+      elem.innerText = elem.innerText.slice(0, 2);
+    });
+  }
+}
+cutLangName();
+window.addEventListener('optimizedResize', function () {
+  cutLangName();
+});
---
+++ webpack.config.js
@@ -0,0 +1,132 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+console.log(isDev);
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: {
+    main: ['@babel/polyfill', '/js/app.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js',
+  },
+
+  devServer: {
+    static: './dist',
+    watchFiles: 'src/**/*',
+    liveReload: false,
+    hot: 'only',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'images', to: 'images' }],
+    }),
+    new SpriteLoaderPlugin({
+      plainSprite: true,
+      spriteAttrs: {
+        fill: '#fff',
+        class: 'svg-icon',
+      },
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: '!!ejs-compiled-loader!/index.ejs',
+      minify: false,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ejs$/,
+        use: {
+          loader: 'ejs-compiled-loader',
+          options: {
+            htmlmin: true,
+            htmlminOptions: {
+              removeComments: true,
+            },
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [require('autoprefixer')],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [require('autoprefixer')],
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /svg\/.*\.svg$/,
+        use: [
+          {
+            loader: 'svg-sprite-loader',
+            options: {
+              extract: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|mp4)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: './images/[name][ext]',
+        },
+      },
+      {
+        test: /\.(ttf|woff|woff2)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: './fonts/[name][ext]',
+        },
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
